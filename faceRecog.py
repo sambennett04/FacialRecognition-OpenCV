63c844f042c486a520feb19b4141ad7acb1c5fe0
Sam Bennett <sbennett@potomacschool.org>
2022-01-04 08:54:19 -0500

    Face Recogniton Screenshot and Image Comparisson

--- faceRecog.py
+++ faceRecog.py
@@ -1,6 +1,6 @@
 #from skimage.metrics import structural_similarity as ssim;
 #import matplotlib.pyplot as plt;
-#import numpy as np
+#mport numpy as np
 import cv2;
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml');
@@ -21,36 +21,37 @@ while True:
     if key == ord('q'):
         break;
 
+video.release();
+cv2.destroyAllWindows();
 
-def mse(imageA, imageB):
+
+#def mse(imageA, imageB):
     # the 'Mean Squared Error' between the two images is the
     # sum of the squared difference between the two images;
     # NOTE: the two images must have the same dimension
-    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-    err /= float(imageA.shape[0] * imageA.shape[1])
+    #err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    #err /= float(imageA.shape[0] * imageA.shape[1])
 
     # return the MSE, the lower the error, the more "similar"
     # the two images are
-    return err
+    #return err
 
-def compare_images(imageA, imageB, title):
+#def compare_images(imageA, imageB, title):
 	# compute the mean squared error and structural similarity
 	# index for the images
-	m = mse(imageA, imageB)
-	s = ssim(imageA, imageB)
+	#m = mse(imageA, imageB)
+	#s = ssim(imageA, imageB)
 	# setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
+	#fig = plt.figure(title)
+	#plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
 	# show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
+	#ax = fig.add_subplot(1, 2, 1)
+	#plt.imshow(imageA, cmap = plt.cm.gray)
+	#plt.axis("off")
 	# show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
+	#ax = fig.add_subplot(1, 2, 2)
+	#plt.imshow(imageB, cmap = plt.cm.gray)
+	#plt.axis("off")
 	# show the images
-	plt.show()
+	#plt.show()
 
-video.release();
-cv2.destroyAllWindows();
